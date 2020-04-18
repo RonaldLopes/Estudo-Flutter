8a28838b25c2e94d6501b498222f463fa1659a07
Ronald Lopes <ronaldlopes@MacBook-de-Ronald.local>
2020-04-18 10:18:24 -0300

    Layout básico da tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,7 +15,33 @@ class LoginPage extends StatelessWidget {
 
   _body() {
     return Container(
-      color: Colors.white,
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text("Login"),
+          TextFormField(),
+          SizedBox(height: 20,),
+          Text("Senha"),
+          TextFormField(
+            obscureText: true,
+          ),
+          SizedBox(height: 20,),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              color: Colors.blue,
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
     );
   }
 }
