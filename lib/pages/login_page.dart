57bdd8fa22e7b4af6dc715bf213abe5d351f7ab8
Ronald Lopes <ronaldlopes@MacBook-de-Ronald.local>
2020-04-18 11:32:35 -0300

    modularizado componentes da tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,49 +18,46 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          TextFormField(
-            style: TextStyle(
-              fontSize: 20
-            ),
-            decoration: InputDecoration(
-              labelText: "Login",
-              labelStyle: TextStyle(
-                fontSize: 20
-              ),
-              hintText: "Digite seu login"
-            ),
-          ),
+          _text("Login", "Informe o login"),
           SizedBox(height: 20,),
-          TextFormField(
-            obscureText: true,
-            style: TextStyle(
-                fontSize: 20
-            ),
-            decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                    fontSize: 20
-                ),
-                hintText: "Digite sa senha"
-            ),
-          ),
+          _text("Senha", "Informe a senha", password: true),
           SizedBox(height: 20,),
-          Container(
-            height: 46,
-            child: RaisedButton(
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              color: Colors.blue,
-              onPressed: () {},
-            ),
-          )
+          _button("Login")
         ],
       ),
     );
+  }
+
+  Container _button(String text) {
+    return Container(
+          height: 46,
+          child: RaisedButton(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            color: Colors.blue,
+            onPressed: () {},
+          ),
+        );
+  }
+
+  TextFormField _text(String text, String hint_text, {bool password = false}) {
+    return TextFormField(
+          obscureText: password ,
+          style: TextStyle(
+            fontSize: 20
+          ),
+          decoration: InputDecoration(
+            labelText: text,
+            labelStyle: TextStyle(
+              fontSize: 20
+            ),
+            hintText: hint_text
+          ),
+        );
   }
 }
