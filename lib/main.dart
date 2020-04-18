c9bb6422803402ab84a81a01800a762c33dcbe43
Ronald Lopes <ronaldlopes@MacBook-de-Ronald.local>
2020-04-18 10:02:15 -0300

    Estrutura básica do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carros/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: LoginPage(),
     );
   }
 }
